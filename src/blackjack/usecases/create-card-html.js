1e369f9b67ee4c1c7f7d9d3cd23f558c184018f3
Uzi Eliel Gonzalezs Ruiz <eliel.gonzalez.r92@gmail.com>
2023-06-22 00:03:50 -0600

    docs:v1.0.0

--- src/blackjack/usecases/create-card-html.js
+++ src/blackjack/usecases/create-card-html.js
@@ -4,7 +4,7 @@ export const createHTMLCard = (carta) =>{
     if(!carta) throw new Error('Card value is necessary.')
 
     const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.src = `/dist/${carta}.png`;
     imgCarta.classList.add('carta');
 
     return imgCarta;
